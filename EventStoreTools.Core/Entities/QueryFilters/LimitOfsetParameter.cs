532fb16c6bf3c683195266bde81a1f7151d4c8bc
Nikolay Garashenko <joshvisler@gmail.com>
2018-03-05 17:04:03 +0200

    Update dependencies

---
+++ EventStoreTools.Core/Entities/QueryFilters/LimitOfsetParameter.cs
@@ -0,0 +1,15 @@
+﻿
+namespace EventStoreTools.Core.Entities.QueryFilters
+{
+    public class LimitOfsetParameter
+    {
+        public LimitOfsetParameter(int limit, int offset)
+        {
+            Limit = limit;
+            Offset = offset;
+        }
+
+        private int Limit { get; }
+        private int Offset { get; }
+    }
+}
---
+++ EventStoreTools.Core/Entities/Restore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EventStoreTools.Core.Entities
+{
+    public class Restore
+    {
+        public Restore()
+        {
+        }
+
+        public Restore(Guid restoreId, Guid clientId, Guid connectionId, string progress, DateTime date)
+        {
+            RestoreId = restoreId;
+            ClientId = clientId;
+            ConnectionId = connectionId;
+            Progress = progress;
+            Date = date;
+        }
+
+        [Key]
+        [Column("restoreid")]
+        public Guid RestoreId { get; private set; }
+        [ForeignKey("Client")]
+        [Column("clientid")]
+        public Guid ClientId { get; private set; }
+        [ForeignKey("Connection")]
+        [Column("connectionid")]
+        public Guid ConnectionId { get; private set; }
+        [Column("progress")]
+        public string Progress { get; private set; }
+        [Column("date")]
+        public DateTime Date { get; set; }
+
+        public Client Client { get; private set; }
+        public Connection Connection { get; private set; }
+    }
+}
