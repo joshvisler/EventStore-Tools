c7a83c62cde76efdfa62946bc613ad96670dbbdf
Nikolay Garashenko <joshvisler@gmail.com>
2018-03-18 14:13:25 +0200

    Add restore controller

---
+++ BackupRestoreService/BackupRestoreService/Controllers/RestoreController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BackupRestoreService.Core.Entities;
+using BackupRestoreService.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackupRestoreService.Controllers
+{
+    [Route("api/[controller]")]
+    public class RestoreController : Controller
+    {
+        private readonly IRestoreService _restoreService;
+
+        public RestoreController(IRestoreService restoreService)
+        {
+            _restoreService = restoreService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Restore>> Get()
+        {
+            return await _restoreService.GetAllRestorsAsync();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody]Guid clientId, int backupId)
+        {
+            if (clientId == Guid.Empty)
+            {
+                return BadRequest("Client not found");
+            }
+
+            return Ok(_restoreService.RestoreAsync(clientId, backupId));
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public async Task Delete([FromBody]int restore)
+        {
+            await _restoreService.DeleteAsync(restore);
+        }
+    }
+}
